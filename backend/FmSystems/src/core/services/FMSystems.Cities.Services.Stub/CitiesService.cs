92ed10ec75788c7072f4828ac164cc9a76391deb
Alvaro Donozo <alvaro.donozo@jalasoft.com>
2019-08-08 13:42:46 -0400

    Implement cities service stub

---
+++ backend/FmSystems/src/core/services/FMSystems.Cities.Services.Stub/CitiesService.cs
@@ -0,0 +1,40 @@
+﻿namespace FMSystems.Cities.Services.Stub
+{
+    using System;
+    using System.Collections.Generic;
+    using FMSystems.Cities.Domain.Models;
+
+    public class CitiesService : ICitiesService
+    {
+        private List<City> cities;
+
+        public CitiesService()
+        {
+            this.PopulateCitiesList();
+        }
+
+        public IList<City> GetCities()
+        {
+            return this.cities;
+        }
+
+        private void PopulateCitiesList()
+        {
+            this.cities = new List<City>
+            {
+                new City
+                {
+                    Id = Guid.Parse("39793810-e0b4-45f2-a7a8-25ed84eec7f3"),
+                    Name = "Phoenix",
+                    State = "AZ"
+                },
+                new City
+                {
+                    Id = Guid.Parse("18ba2d82-2c0c-44bd-8ca9-78001064fcad"),
+                    Name = "Raleigh",
+                    State = "NC"
+                }
+            };
+        }
+    }
+}
--- backend/FmSystems/src/core/services/FMSystems.Cities.Services.Stub/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace FMSystems.Cities.Services.Stub
-{
-    public class Class1
-    {
-    }
-}
