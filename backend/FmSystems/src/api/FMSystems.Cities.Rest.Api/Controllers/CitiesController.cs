1f3a615ce8a86442a2f80cc54575d7e9b1ea352f
Alvaro Donozo <alvaro.donozo@jalasoft.com>
2019-08-08 13:54:33 -0400

    Add cities controller

---
+++ backend/FmSystems/src/api/FMSystems.Cities.Rest.Api/Controllers/CitiesController.cs
@@ -0,0 +1,28 @@
+﻿namespace FMSystems.Cities.Rest.Api.Controllers
+{
+    using FMSystems.Cities.Domain.Models;
+    using FMSystems.Cities.Services;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+    using System.Collections.Generic;
+
+    [ApiController]
+    [Route("api/v1/")]
+    public class CitiesController : ControllerBase
+    {
+        private ICitiesService citiesService;
+
+        public CitiesController(ICitiesService citiesService)
+        {
+            this.citiesService = citiesService;
+        }
+
+        [Route("cities")]
+        [HttpGet]
+        public ActionResult<IList<City>> GetCities()
+        {
+            var cities = this.citiesService.GetCities();
+            return this.StatusCode(StatusCodes.Status200OK, cities);
+        }
+    }
+}
