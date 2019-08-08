3e724d1cfd6408ba8d7b27bb50869aa5953044c7
Alvaro Donozo <alvaro.donozo@jalasoft.com>
2019-08-08 13:54:23 -0400

    Add unit test for cities controller

---
+++ backend/FmSystems/tests/api/FMSystems.Cities.Rest.Api.Tests/CitiesControllerTests.cs
@@ -0,0 +1,26 @@
+namespace FMSystems.Cities.Rest.Api.Tests
+{
+    using FMSystems.Cities.Rest.Api.Controllers;
+    using FMSystems.Cities.Services.Stub;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc.Infrastructure;
+    using Xunit;
+
+    public class CitiesControllerTests
+    {
+        private CitiesController controller;
+
+        public CitiesControllerTests()
+        {
+            this.controller = new CitiesController(new CitiesService());
+        }
+
+        [Fact]
+        public void TestGetCities_ReturnsStatusCodeOK()
+        {
+            var actual = this.controller.GetCities().Result as IStatusCodeActionResult;
+            var expected = StatusCodes.Status200OK;
+            Assert.Equal(expected, actual.StatusCode);
+        }
+    }
+}
