6fbf6d5eb01c6cb4ed2ee46bf2acc50fe7b8de6f
Alvaro Donozo <alvaro.donozo@jalasoft.com>
2019-08-08 13:36:40 -0400

    Add city model

---
+++ backend/FmSystems/src/core/domain/FMSystems.Cities.Domain.Models/City.cs
@@ -0,0 +1,17 @@
+﻿namespace FMSystems.Cities.Domain.Models
+{
+    using System;
+
+    public class City
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string State { get; set; }
+
+        public float Latitude { get; set; }
+
+        public float Longitude { get; set; }
+    }
+}
--- backend/FmSystems/src/core/domain/FMSystems.Cities.Domain.Models/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace FMSystems.Cities.Domain.Models
-{
-    public class Class1
-    {
-    }
-}
