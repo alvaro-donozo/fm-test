20cbc3038c57c557574b9d2cff93b5a478103df9
Alvaro Donozo <alvaro.donozo@jalasoft.com>
2019-08-08 14:30:22 -0400

    Implement cities service

--- frontend/fm-systems/src/app/cities/services/cities.service.ts
+++ frontend/fm-systems/src/app/cities/services/cities.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpService } from '../../core/services/http.service';
+import { Observable } from 'rxjs';
+
+import { City } from '../../core/models/city';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CitiesService {
+  private resource = 'cities';
 
-  constructor() { }
+  constructor(private httpService: HttpService) { }
+
+  public getCities(): Observable<City[]> {
+    return this.httpService.get(this.resource);
+  }
 }
