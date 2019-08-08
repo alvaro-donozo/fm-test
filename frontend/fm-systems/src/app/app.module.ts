7714e9a62387faf5653499d81aed21e79ed7cb0f
Alvaro Donozo <alvaro.donozo@jalasoft.com>
2019-08-08 14:26:38 -0400

    Add cities component base files

--- frontend/fm-systems/src/app/app.module.ts
+++ frontend/fm-systems/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CitiesComponent } from './cities/cities.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CitiesComponent
   ],
   imports: [
     BrowserModule,
