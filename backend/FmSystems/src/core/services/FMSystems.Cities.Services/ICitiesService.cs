16214ea1e579f35b2d05c9f9c7e057049e8d77bf
Alvaro Donozo <alvaro.donozo@jalasoft.com>
2019-08-08 13:38:07 -0400

    Add cities service interface

---
+++ backend/FmSystems/src/core/services/FMSystems.Cities.Services/ICitiesService.cs
@@ -0,0 +1,10 @@
+﻿namespace FMSystems.Cities.Services
+{
+    using FMSystems.Cities.Domain.Models;
+    using System.Collections.Generic;
+
+    public interface ICitiesService
+    {
+        IList<City> GetCities();
+    }
+}
