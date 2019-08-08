d3f9eaff2756eb4b5dbcdbd7f2f88a305f73c4e7
Alvaro Donozo <alvaro.donozo@jalasoft.com>
2019-08-08 13:11:16 -0400

    Add rest server project

---
+++ backend/FmSystems/api/FMSystems.Cities.Rest.Server/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace FMSystems.Cities.Rest.Server
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
