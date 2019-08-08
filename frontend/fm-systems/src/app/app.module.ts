2e0d14cfe8dab4e1c4c0a93fa5c57044ba3a7c41
Alvaro Donozo <alvaro.donozo@jalasoft.com>
2019-08-08 14:35:30 -0400

    Add cities component in app component

--- frontend/fm-systems/src/app/app.module.ts
+++ frontend/fm-systems/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CitiesComponent } from './cities/cities.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { CitiesComponent } from './cities/cities.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
