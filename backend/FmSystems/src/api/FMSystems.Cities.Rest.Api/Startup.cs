9f9dd3a39db1d151b588c0e8443595884ae495f3
Alvaro Donozo <alvaro.donozo@jalasoft.com>
2019-08-08 13:31:19 -0400

    Startup class for rest api

---
+++ backend/FmSystems/src/api/FMSystems.Cities.Rest.Api/Startup.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FMSystems.Cities.Rest.Api
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
+            app.UseMvc();
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors()
+                .AddMvcCore()
+                .AddFormatterMappings()
+                .AddJsonFormatters()
+                .AddJsonOptions(options =>
+                    options.SerializerSettings.NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore
+                );
+        }
+    }
+}
