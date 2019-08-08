cc474adb4114e748c2e3a941d1f033095e4012ca
Alvaro Donozo <alvaro.donozo@jalasoft.com>
2019-08-08 14:00:09 -0400

    Add memory cities dao

---
+++ backend/FmSystems/src/dao/FMSystems.Cities.Dao.Memory/CitiesDao.cs
@@ -0,0 +1,52 @@
+﻿namespace FMSystems.Cities.Dao.Memory
+{
+    using System;
+    using System.Collections.Generic;
+    using FMSystems.Cities.Domain.Models;
+
+    public class CitiesDao : ICitiesDao
+    {
+        private List<City> cities;
+
+        public CitiesDao()
+        {
+            this.PopulateCitiesList();
+        }
+
+        public IList<City> GetCities()
+        {
+            return this.cities;
+        }
+
+        private void PopulateCitiesList()
+        {
+            this.cities = new List<City>
+            {
+                new City
+                {
+                    Id = Guid.Parse("573f0e0c-7987-420a-b885-346cbf2972ab"),
+                    Name = "Phoenix",
+                    State = "AZ"
+                },
+                new City
+                {
+                    Id = Guid.Parse("fe55d066-e1db-4996-8038-0a517536066b"),
+                    Name = "Raleigh",
+                    State = "NC"
+                },
+                new City
+                {
+                    Id = Guid.Parse("04b1b89d-297d-4cad-aa1b-ad2b96cef5c9"),
+                    Name = "Saint John",
+                    State = "NB"
+                },
+                new City
+                {
+                    Id = Guid.Parse("2666a649-900a-4e9b-9739-431858a68916"),
+                    Name = "San Diego",
+                    State = "CA"
+                }
+            };
+        }
+    }
+}
--- backend/FmSystems/src/dao/FMSystems.Cities.Dao.Memory/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace FMSystems.Cities.Dao.Memory
-{
-    public class Class1
-    {
-    }
-}
