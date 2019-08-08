5c78b62c58d32dde89d623fa0ac3ba9e7a9c1908
Alvaro Donozo <alvaro.donozo@jalasoft.com>
2019-08-08 14:24:34 -0400

    Add http service implementation

--- frontend/fm-systems/src/app/core/services/http.service.ts
+++ frontend/fm-systems/src/app/core/services/http.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpService {
+  private serviceUrl = 'http://localhost:8000/api/v1/';
 
-  constructor() { }
+  constructor(private httpService: HttpClient) { }
+
+  public get<T>(resource: string): Observable<T> {
+    return this.httpService.get<T>(this.serviceUrl + resource);
+  }
 }
